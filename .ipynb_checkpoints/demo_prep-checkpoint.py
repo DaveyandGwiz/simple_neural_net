a3c366f83a7dcdd3bbc77ab71c5540f7d58f0aed
David Galloway <daveyandgwiz@gmail.com>
2025-05-13 12:55:45 -0500

    demo

---
+++ .ipynb_checkpoints/demo_prep-checkpoint.py
@@ -0,0 +1,27 @@
+The perceptron and into to ml
+
+Neural nets are machine learning models inspired by the way the brain processes
+if infomation.
+
+In the brani, the fundamental unit of computation is a neuron
+A neuron take in several inputs and based on the stengths of those inputs
+and activation threshold either does or does not transmit a signal output to other neurons.
+
+In neaural nets, the funamantal unit is a perceptron which is comparable to a
+if nueron:
+
+Mathematically a perceptron can be modeled in the following fashion
+
+ The inputs (these are the features that the model takes in): x, x1, xn
+ The weights (these are the multipliers applied to inputs): w1,w2,...wn
+ The Bias which is an additional constant applied to the sum
+ F(x) or the activation function. This is a step function producing a binry output based on a threshgold.
+
+
+There is a limit to what can be learned by a single perceptron.
+
+The perceptron's step function onl
+
+
+
+
